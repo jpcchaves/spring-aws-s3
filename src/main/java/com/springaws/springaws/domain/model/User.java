bca3abca91307e1b011067795b245f4940738c4d
João Chaves <joao.chaves@liferay.com>
2024-02-10 16:24:31 -0300

    feat: add user entity

---
+++ src/main/java/com/springaws/springaws/domain/model/User.java
@@ -0,0 +1,94 @@
+package com.springaws.springaws.domain.model;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 150, nullable = false)
+    private String firstName;
+
+    @Column(length = 150, nullable = false)
+    private String lastName;
+
+    @Column(length = 150, nullable = false, unique = true)
+    private String email;
+
+    @Column(length = 11, nullable = false, unique = true)
+    private String cpf;
+
+    @Column(length = 36, unique = true)
+    private UUID profileImage;
+
+    public User() {
+    }
+
+    public User(Long id,
+                String firstName,
+                String lastName,
+                String email,
+                String cpf,
+                UUID profileImage) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.cpf = cpf;
+        this.profileImage = profileImage;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public UUID getProfileImage() {
+        return profileImage;
+    }
+
+    public void setProfileImage(UUID profileImage) {
+        this.profileImage = profileImage;
+    }
+}
