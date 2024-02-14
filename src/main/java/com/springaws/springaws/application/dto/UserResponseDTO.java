8e68068f58c94db17eb73b05058ca62d16e37abf
João Chaves <joao.chaves@liferay.com>
2024-02-14 11:03:16 -0300

    feat: add attributes of user response dto

--- src/main/java/com/springaws/springaws/application/dto/UserResponseDTO.java
+++ src/main/java/com/springaws/springaws/application/dto/UserResponseDTO.java
@@ -1,4 +1,55 @@
 package com.springaws.springaws.application.dto;
 
+import org.hibernate.validator.constraints.br.CPF;
+
 public class UserResponseDTO {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String cpf;
+
+    public UserResponseDTO() {
+    }
+
+    public UserResponseDTO(String firstName,
+                           String lastName,
+                           String email,
+                           String cpf) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.cpf = cpf;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 }
