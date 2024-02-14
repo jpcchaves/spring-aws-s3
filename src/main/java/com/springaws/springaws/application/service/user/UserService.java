71fa135c6c5cd19a3b30bf2182ca8387fa2c853a
João Chaves <joao.chaves@liferay.com>
2024-02-14 11:08:01 -0300

    feat: add crud methods in user service interface

--- src/main/java/com/springaws/springaws/application/service/user/UserService.java
+++ src/main/java/com/springaws/springaws/application/service/user/UserService.java
@@ -1,4 +1,14 @@
 package com.springaws.springaws.application.service.user;
 
+import com.springaws.springaws.application.dto.UserRequestDTO;
+import com.springaws.springaws.application.dto.UserResponseDTO;
+
+import java.util.List;
+
 public interface UserService {
+    List<UserResponseDTO> getUsersList();
+    UserResponseDTO getUserById(Long userId);
+    UserResponseDTO createUser(UserRequestDTO requestDTO);
+    UserResponseDTO updateUser(Long userId, UserRequestDTO requestDTO);
+    void deleteUser(Long userId);
 }
